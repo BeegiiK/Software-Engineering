7f3a5df34dfd60ce761f574a46c975555176351f
BeegiiK <kbeegii@gmail.com>
2021-11-10 12:42:15 +0000

    10/11/2021 - BK (Setting up initial packages) - NA

--- CatanJunior/src/Player/player.java
+++ CatanJunior/src/Player/player.java
@@ -1,5 +1,14 @@
 package Player;
 
 public class player {
-
+	
+	private Colour playerColour;
+	private boolean playerTurn = false;
+	Hashtable<String, int> playerResources = new Hashtable<String, int>();
+	
+	private Player(Colour colour, Hashtable<String, int> resources) {
+		this.playerColour = colour;
+	}
+	
+	
 }
