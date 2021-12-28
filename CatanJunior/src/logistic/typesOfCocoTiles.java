05e33b24b7144b6d431163c55bbd4607023d4674
Derek Sequeira <wikorecorder@gmail.com>
2021-12-28 22:23:07 +0000

    SYNC

--- CatanJunior/src/logistic/typesOfCocoTiles.java
+++ CatanJunior/src/logistic/typesOfCocoTiles.java
@@ -1,9 +1,20 @@
 package logistic;
+import map.Colour;
+import map.RESOURCE;
 
 public enum typesOfCocoTiles {
 	//only 20 coco tiles
-	MOVEGHOSTPIRATE,
-	SHIP_LAIR,
-	GOAT_CUTLASSES,
-	MOLASSES_WOOD, 
+	MOVEGHOSTPIRATE("Move the Ghost Pirate"),
+	SHIP_LAIR("Get a Ship or a Lair"),
+	GOAT_CUTLASSES("Get x2 Goats & x2 Cutlasses"),
+	MOLASSES_WOOD("Get x2 Molasses & x2 Wood");
+	
+	public final String description;
+	private typesOfCocoTiles(String description) {
+		this.description = description;
+	}
+	
+    public static String getDescription(typesOfCocoTiles C) {
+        return C.description;
+    }
 }
